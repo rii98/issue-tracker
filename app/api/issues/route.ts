c4e8f8fbd9cd3505a80765d0a75026d09cb16836
rii98 <136151503+rii98@users.noreply.github.com>
2023-12-23 22:33:10 +0545

    Build an api for creating a issue

---
+++ app/api/issues/route.ts
@@ -0,0 +1,29 @@
+import { create } from "domain";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from "@/prisma/client";
+const CreateIssueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+  status: z.enum(["OPEN", "IN_PROGRESS", "CLOSED"]),
+});
+
+export async function POST(request: NextRequest) {
+  const data = await request.json();
+  const validation = CreateIssueSchema.safeParse(data);
+  if (!validation.success) {
+    return NextResponse.json(
+      { error: validation.error.errors },
+      { status: 400 }
+    );
+  }
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: data.title,
+      description: data.description,
+      status: data.status,
+    },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
+}
