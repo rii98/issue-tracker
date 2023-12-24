592684c097e52188df5ca62d2d4d622ec4288c0c
rii98 <136151503+rii98@users.noreply.github.com>
2023-12-24 13:24:24 +0545

    Adding mark down editor to create new issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
-import { Theme, ThemePanel } from "@radix-ui/themes";
+import { Theme } from "@radix-ui/themes";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -24,7 +24,6 @@ export default function RootLayout({
         <Theme appearance="light" accentColor="iris">
           <NavBar />
           <main className="p-4">{children}</main>
-          <ThemePanel />
         </Theme>
       </body>
     </html>
