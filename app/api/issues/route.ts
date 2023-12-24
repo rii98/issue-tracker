3a12f04847777f927bed75acf82c5a75c7e04ed6
rii98 <136151503+rii98@users.noreply.github.com>
2023-12-24 16:03:25 +0545

    Create new issue page
    We used react-hook-form and axios for this

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -7,13 +7,15 @@ import prisma from "@/prisma/client";
 const CreateIssueSchema = z.object({
   title: z.string().min(1).max(255),
   description: z.string().min(1),
-  status: z.enum(["OPEN", "IN_PROGRESS", "CLOSED"]),
+  status: z.enum(["OPEN", "IN_PROGRESS", "CLOSED"]).optional(),
 });
 
 // CREATE A NEW ISSUE
 export async function POST(request: NextRequest) {
   const data = await request.json();
+  console.log(data);
   const validation = CreateIssueSchema.safeParse(data);
+
   if (!validation.success) {
     return NextResponse.json(
       { error: validation.error.errors },
--- app/issue/new/page.tsx
+++ app/issue/new/page.tsx
@@ -1,18 +1,37 @@
 "use client";
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button, TextField } from "@radix-ui/themes";
 import React from "react";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+type IssueForm = {
+  title: String;
+  description: String;
+};
 
 const CreateNewIssue = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const onSubmit = async (data: IssueForm) => {
+    const response = await axios.post("http://localhost:3000/api/issues", data);
+    if (response.status === 201) router.push("/issue");
+  };
   return (
-    <div className="max-w-xl space-y-4">
+    <form className="max-w-xl space-y-4" onSubmit={handleSubmit(onSubmit)}>
       <TextField.Root>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input placeholder="Title" {...register("title")} />
       </TextField.Root>
-      <SimpleMDE placeholder="Description..." />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description..." {...field} />
+        )}
+      />
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
